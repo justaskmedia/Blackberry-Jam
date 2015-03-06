2eb08e81a21ba5bd5334f4c085669822ded8407b
Andrew Kelly <akelly@justaskmedia.ca>
2015-03-05 23:33:04 -0500

    Added Bootstrap

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('build', ['jsmin', 'sass', 'copy', 'browser-sync', 'images']);
+gulp.task('build', ['jsmin', 'sass', 'build-css', 'copy', 'browser-sync', 'images']);
--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 gulp.task('default', ['build'], function() {
 	gulp.watch("templates/**/*", ['copy']);
-	gulp.watch("assets/sass/**/*", ['sass']);
+	gulp.watch("assets/sass/**/*", ['sass', 'build-css']);
 	gulp.watch("assets/js/**/*", ['jsmin']);
 	gulp.watch("assets/images/**/*", ['images']);
 });
