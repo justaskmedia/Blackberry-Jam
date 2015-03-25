5234a248c1cef9ee9e1964e1325e39ea4a10d166
Andrew <andrew@idearevolution.ca>
2015-03-25 14:26:04 -0400

    This is the build-css fix for the gulp file.

--- gulp/tasks/build-css.js
+++ gulp/tasks/build-css.js
@@ -4,19 +4,33 @@ var uglifycss = require('gulp-uglifycss');
 var concat = require('gulp-concat');
 var filter = require('gulp-filter');
 var order = require("gulp-order");
+var clean = require("gulp-clean");
 
-gulp.task('build-css', function() {
-	var cssFiles = ['./build/assets/css/*'];
-	var dest = './build/assets/';
+gulp.task('build-css', ['clean-scripts'], function() {
+    var cssFiles = ['./build/assets/css/*'];
+    var dest = './build/assets/';
+    var bowerFiles = mainBowerFiles({
+        paths: {
+            bowerDirectory: 'bower_components/'
+        }
+    });
+    console.log('bower files: ', bowerFiles);
 
-	gulp.src(mainBowerFiles().concat(cssFiles))
-		.pipe(filter('*.css'))
-		.pipe(order([
-			'normalize.css',
-			'bootstrap',
-			'*'
-		]))
-		.pipe(concat('main.css'))
-		.pipe(uglifycss())
-		.pipe(gulp.dest(dest + 'css'));
+    gulp.src(mainBowerFiles().concat(cssFiles))
+        .pipe(filter('*.css'))
+        .pipe(order([
+            'normalize.css',
+            'bootstrap',
+            '*'
+        ]))
+        .pipe(concat('main.min.css'))
+        .pipe(uglifycss())
+        .pipe(gulp.dest(dest + 'css'));
+});
+
+gulp.task('clean-scripts', function () {
+    var dest = './build/assets/css/';
+    console.log(dest + 'main.min.css')
+      return gulp.src(dest + 'main.min.css', {read: false})
+        .pipe(clean());
 });
